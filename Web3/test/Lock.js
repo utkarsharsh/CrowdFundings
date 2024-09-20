fcb1e6fd62ec2445592c67c2621d251bf4a91b14
utkarsharsh <harshupadhyay7786@gmail.com>
2024-09-20 13:19:55 +0530

    first push with blockchain abi

---
+++ Web3/test/Lock.js
@@ -0,0 +1,22 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+
+describe("CrowdFunding", function () {
+
+
+    
+
+ 
+  it("checking it brother", async function () {
+   expect ("Aa").to.equal("Aa");
+  });
+         
+
+  
+
+
+});
